39e58e0a584d71854ff73ba87f6db68f15aa4a97
abdel <abdeljalilhartani65@gmail.com>
2021-12-30 14:56:03 +0100

    setting up for basic node and express

---
+++ MyApp.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var bGround = require('fcc-express-bground');
+
+
+
+bGround.log('Hello World');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
